75e2b13784dce9147767473aadbe9cf922233ac5
Rojan Mainali <rojanmainali@Rojans-MacBook-Air.local>
2026-07-30 19:37:14 +0545

    light

--- lib/core/services/light/light_sensor_service.dart
+++ lib/core/services/light/light_sensor_service.dart
@@ -1,9 +1,41 @@
 import 'dart:async';
-import 'package:light_sensor/light_sensor.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class LightSensorService {
-  Stream<int> getLuxStream() => LightSensor.luxStream();
+  static const _channel = EventChannel('aqua_life/light_sensor');
+
+  Stream<int> getLuxStream() {
+    final controller = StreamController<int>();
+    Timer? pendingTimer;
+    int? latestValue;
+    bool done = false;
+
+    _channel.receiveBroadcastStream().listen(
+      (event) {
+        final value = (event as int?) ?? 0;
+        latestValue = value;
+        pendingTimer?.cancel();
+        pendingTimer = Timer(const Duration(milliseconds: 500), () {
+          if (latestValue != null && !done) {
+            controller.add(latestValue!);
+            latestValue = null;
+          }
+        });
+      },
+      onError: controller.addError,
+      onDone: () {
+        done = true;
+        pendingTimer?.cancel();
+        if (latestValue != null) {
+          controller.add(latestValue!);
+        }
+        controller.close();
+      },
+    );
+
+    return controller.stream;
+  }
 
   bool isDarkFromLux(int lux) => lux <= 10;
 }
